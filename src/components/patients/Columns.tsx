37181fe3a210c6ad6299c584da19351f7634b8e6
Marcel Gatete <mgatete@bk.rw>
2024-01-04 12:02:32 +0200

    fix: fix left and right eye patient view and add action buttons

--- src/components/patients/Columns.tsx
+++ src/components/patients/Columns.tsx
@@ -3,6 +3,21 @@
 import { IPatient } from "@/types/patient.types";
 import { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
+import { Edit2Icon, DeleteIcon, ShoppingBagIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { ServiceStatus } from "@prisma/client";
+
+const handleEdit = (row: any) => {
+  console.log(row);
+};
+
+const handleDelete = (row: any) => {
+  console.log(row);
+};
+
+const handleOrder = (row: any) => {
+  console.log(row);
+};
 
 export const columns: ColumnDef<IPatient>[] = [
   {
@@ -16,14 +31,18 @@ export const columns: ColumnDef<IPatient>[] = [
   {
     accessorKey: "name",
     header: "Name",
+    cell: ({ getValue }) => {
+      const value = "" + getValue();
+      return <p className="text-primary font-semibold">{value}</p>;
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
-    cell: ({ getValue }) => {
-      const value = "" + getValue();
-      return <p className="text-primary">{value}</p>;
-    },
+    // cell: ({ getValue }) => {
+    //   const value = "" + getValue();
+    //   return <p className="text-primary">{value}</p>;
+    // },
   },
   {
     accessorKey: "phone",
@@ -40,18 +59,75 @@ export const columns: ColumnDef<IPatient>[] = [
   {
     accessorKey: "rightEye",
     header: "Right Eye",
-    // cell: ({ getValue }) => {
-    //   const value = "" + getValue();
-    //   return JSON.parse(value);
-    // ?.map((item: any) => (
-    // <p key={item.name} className="text-primary">
-    //   {item.name} : {item.value}
-    // </p>
-    // ));
-    // },
+    cell: ({ getValue }: { getValue: any }) => {
+      const value = JSON.parse("" + getValue());
+      return Object.keys(value).map((key) => {
+        return (
+          <div className="flex items-center gap-2 font-semibold" key={key}>
+            <p className="capitalize">{key}</p>
+            <p className="">{value[key]}</p>
+          </div>
+        );
+      });
+    },
   },
   {
     accessorKey: "leftEye",
     header: "Left Eye",
+    cell: ({ getValue }: { getValue: any }) => {
+      const value = JSON.parse("" + getValue());
+      return Object.keys(value).map((key) => {
+        return (
+          <div className="flex items-center gap-2 font-semibold" key={key}>
+            <p className="capitalize">{key}</p>
+            <p className="">{value[key]}</p>
+          </div>
+        );
+      });
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ getValue }) => {
+      const value = ("" + getValue()) as ServiceStatus;
+      return (
+        <div
+          className={`${
+            value === "WAITING_FOR_GLASSES"
+              ? "bg-orange-100 text-orange-400"
+              : value === "GLASSES_READY"
+              ? "bg-sky-100 text-sky-500"
+              : value === "GLASSES_PICKED_UP"
+              ? "bg-green-100 text-green-600"
+              : ""
+          } rounded py-1 px-3 capitalize text-sm`}
+        >
+          {value.replaceAll("_", " ").toLowerCase()}
+        </div>
+      );
+    },
+  },
+  {
+    header: "Actions",
+    cell: ({ row }) => {
+      return (
+        <div className="flex items-center gap-2">
+          <Button
+            variant="ghost"
+            className="p-0"
+            onClick={() => handleEdit(row)}
+          >
+            <Edit2Icon />
+          </Button>
+          <Button variant="ghost" className="p-0">
+            <DeleteIcon className="text-red-500" />
+          </Button>
+          <Button variant="ghost" className="p-0">
+            <ShoppingBagIcon className="text-primary" />
+          </Button>
+        </div>
+      );
+    },
   },
 ];
