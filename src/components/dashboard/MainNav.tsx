5d5f642c911612d1cc7c81c4a45b3e9ad255ae44
Marcel Gatete <mgatete@bk.rw>
2024-01-04 13:36:49 +0200

    refactor: refactor dashboard left sidebar links

--- src/components/dashboard/MainNav.tsx
+++ src/components/dashboard/MainNav.tsx
@@ -1,62 +1,58 @@
+"use client";
 import Link from "next/link";
-
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import {
+  GlassesIcon,
+  LayoutDashboard,
+  UserSquareIcon,
+  Settings2Icon,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const MainNavLinks = [
+  {
+    name: "Overview",
+    path: "/dashboard",
+    icon: <LayoutDashboard size={30} />,
+  },
+  {
+    name: "Patients",
+    path: "/dashboard/patients",
+    icon: <UserSquareIcon size={30} />,
+  },
+  {
+    name: "Glasses",
+    path: "/dashboard/glasses",
+    icon: <GlassesIcon size={30} />,
+  },
+  {
+    name: "Settings",
+    path: "/dashboard/settings",
+    icon: <Settings2Icon size={30} />,
+  },
+];
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const pathname = usePathname();
+
   return (
     <nav className={cn("flex flex-col gap-5", className)} {...props}>
-      <Link
-        href="/dashboard"
-        className="font-medium transition-colors hover:text-primary flex items-center justify-start gap-3 text-lg"
-      >
-        <Image
-          src="/assets/icons/dashboard.svg"
-          alt="Dashboard"
-          width={30}
-          height={30}
-        />
-        Overview
-      </Link>
-      <Link
-        href="/dashboard/patients"
-        className="font-medium text-muted-foreground transition-colors hover:text-primary flex items-center justify-start gap-3 text-lg"
-      >
-        <Image
-          src="/assets/icons/patients.svg"
-          alt="patients"
-          width={30}
-          height={30}
-        />
-        Patients
-      </Link>
-      <Link
-        href="/dashboard/glasses"
-        className="font-medium text-muted-foreground transition-colors hover:text-primary flex items-center justify-start gap-3 text-lg"
-      >
-        <Image
-          src="/assets/icons/eyeglass.svg"
-          alt="eyeglass"
-          width={30}
-          height={30}
-        />
-        Glasses
-      </Link>
-      <Link
-        href="/dashboard/settings"
-        className="font-medium text-muted-foreground transition-colors hover:text-primary flex items-center justify-start gap-3 text-lg"
-      >
-        <Image
-          src="/assets/icons/settings.svg"
-          alt="settings"
-          width={30}
-          height={30}
-        />
-        Settings
-      </Link>
+      {MainNavLinks.map((link) => (
+        <Link
+          key={link.name}
+          href={link.path}
+          className={cn(
+            "font-medium transition-colors flex items-center justify-start gap-3 text-lg",
+            pathname === link.path && "text-primary"
+          )}
+        >
+          {link.icon}
+          {link.name}
+        </Link>
+      ))}
     </nav>
   );
 }
